e21a8f6c8153877af0fc3eea7c7ab1ac562e9d60
Milton Rodrigues <rodriguesmilton21@gmail.com>
2022-03-19 22:25:50 +0530

    -

--- routes/movies.js
+++ routes/movies.js
@@ -19,4 +19,56 @@ router.post("/", verify, async (req, res) => {
   }
 });
 
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updatedMovie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(201).json(updatedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed! "); //if he is not a user
+  }
+});
+
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await Movie.findByIdAndDelete(req.params.id);
+      res.status(201).json("The Movie has been deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed! "); //if he is not a user
+  }
+});
+
+router.get("/:id", verify, async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    res.status(201).json(movie);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get random movie
+router.get("/random", verify, async (req, res) => {
+  const type = req.query.type;
+  let movie;
+  try {
+    if (type === "series") {
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
